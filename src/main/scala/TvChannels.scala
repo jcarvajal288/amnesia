6b29cfb1fd3e3fae5140c302c7c5fa3aa377da44
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-21 19:05:04 -0600

    Initial commit.  Finished Node1Text

---
+++ src/main/scala/TvChannels.scala
@@ -0,0 +1,76 @@
+object TvChannels {
+
+  val channel2: String =
+    """
+      |  On Channel 2 there is an ad for Kool-Aid, and then a re-run of
+      |WHEEL OF FORTUNE resumes. The three contestants are trying to
+      |guess the letters of someone's name. There's no T in it, no S,
+      |no N...
+      |""".stripMargin
+
+  val channel4: String =
+    """
+      |  Channel 4 has a news program. The President of El Salvador
+      |wants more money for his country's defense. Reagan and the
+      |Soviet Union have unkind things to say about each other. Two
+      |people died in a fire in the Bronx. The weather will remain
+      |sunny.
+      |""".stripMargin
+
+  val channel5: String =
+    """
+      |  Channnel 5 has ads for soap and toothpaste and floor wax, and
+      |then a talk show host resumes his interview with an actress
+      |starring in a new prime-time soap opera, who feels that her role
+      |is helping her to grow in unexpected directions.
+      |""".stripMargin
+
+  val channel7: String =
+    """
+      |  Channel 7 also has a news program. The Soviet Union says
+      |that Reagan is obstructing plans for disarmament talks, but Reagan
+      |says it's the Soviets who are to blame. The President of El
+      |Salvador has asked Congress for money for arms. Two people died
+      |in a fire in the Bronx. The weather is bright and sunny.
+      |""".stripMargin
+
+  val channel9: String =
+    """
+      |  Channel 9 is showing a re-run of FAMILY FEUD. Host Richard
+      |Dawson is speaking: "100 people surveyed, top five answers are
+      |on the board--here's the question: Name something you would
+      |expect to find in a pawn shop window." The question seems to
+      |ring a bell.  Is it possible you've seen this same program before?
+      |Before either contestant can answer, you take up the ballpoint
+      |pen and jot down what you think the number-one answer will be.
+      |""".stripMargin
+
+  val channel9a: String =
+    """
+      |The first contestant says "Jewelry" which turns out to be the
+      |number-three answer. The other contestant says "Cameras," and
+      |that's number five. "Watch" turns out to be the number one
+      |answer. You continue watching the program, and every answer
+      |seems familiar. Your favorite color of jellybean: red. A food
+      |that children refuse to eat: liver. The number of hours per day
+      |you watch tv: three (although one contestant lays claim to six).
+      |""".stripMargin
+
+  val channel11: String =
+    """
+      |  Channel 11 is showing a lot of advertising interspersed with
+      |clips from an old black-and-white movie.
+      |""".stripMargin
+
+  val channel13: String =
+    """
+      |  Channel 13 is playing SESAME STREET. Today's sponsors are the
+      |letters M and N, and the number 9.
+      |""".stripMargin
+
+  val channel2repeat: String =
+    """
+      |  You are back at Channel 2, but the cumulative effect of so much
+      |daytime televions overwhelms you. You feel yourself nodding off.
+      |""".stripMargin
+}
