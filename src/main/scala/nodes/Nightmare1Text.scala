fb584f0b54a2a518f06c44df1e6aa98fe8d2e9b1
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-25 21:25:20 -0600

    Added text class for Nightmare 1

---
+++ src/main/scala/nodes/Nightmare1Text.scala
@@ -0,0 +1,409 @@
+package nodes
+
+object Nightmare1Text {
+
+  val text1: String =
+    """
+      |You are dreaming. You are dreaming that you have been asleep
+      |and that you wake to find yourself in a strange hotel.
+      |The only light in the room comes from the hotel 's gigantic
+      |neon light that glows a baleful red outside the window. "X,"
+      |a voice whispers in the crimson twilight, "X, are you there?"
+      |You know that you are X and that you must answer the voice
+      |truthfully, but your mouth is dr y , your tongue paralyzed with
+      |fear. "Come here, X," the voice insists. "Come here to me,
+      |in the mirror."
+      |""".stripMargin
+
+  val text1a: String =
+    """
+      |You tell yourself to ..... (response) ...... 1,
+      |but something prevents you. Your acts seem not to be your
+      |own. And the voice repeats its command: "Come here, X.
+      |Come here to me, in the mirror.
+      |""".stripMargin
+
+  val text2: String =
+    """
+      |Obedient to the voice, you go to the mirror. The figure in
+      |the mirror leans forward to peer at you intently. He is
+      |dressed all in white, like a bridegroom or a ghost. And
+      |though he has no face--only eyes that stare anxiously from
+      |the smooth ovoid of his head--he smiles, recognizing you.
+      |"Excellent," he whispers. "Now come with me--before the
+      |store closes." In the mirror you see him turn away from you
+      |and walk toward the door of the room, where he pauses to look
+      |back at you, and to beckon, with his raised hand, for you to
+      |follow.
+      |""".stripMargin
+
+  val text2a: String =
+    """
+      |You cannot follow him without entering the mirror.
+      |""".stripMargin
+
+  val text3: String =
+    """
+      |As you enter the mirror, the beckoning figure vanishes. You
+      |follow him out of the room and catch another glimpse of him
+      |at the far end of the corridor. You run toward him and reach
+      |his side just as the subway is pulling into the station. The
+      |doors open with a shudder. "Come," says the faceless figure,
+      |putting his arm about your shoulder. "You mustn 't be late
+      |your first day at work." If you wished to, you could not
+      |resist his greater strength. You enter the empty subway car.
+      |"Quickly!" Your companion hands you a spray can of black
+      |enamel. "Before the police come and you 're arrested--write a
+      |graffito. Quickly!" You aim the can at the the one window
+      |of the subway car that is not already a palimpsest of disposable
+      |identities. Then you press the nozzle and write:
+      |""".stripMargin
+
+  val text4: String =
+    """
+      |No sooner have you sprayed your offensive message on the
+      |subway car 's window, than Mayor Koch bursts upon the scene,
+      |with an entourage including two policemen, a press
+      |photographer, and the head of the Mayors Commission to Keep
+      |the Subways Clean, who is no less a celebrity than (name).
+      |""".stripMargin
+
+  val text5: String =
+    """
+      |The press photographer takes a picture of you standing
+      |handcuffed between the two policemen in front of the
+      |offending graffito. "Ladies and gentlemen," the Mayor
+      |announces. "Today we eliminate once and for all the problem
+      |of graffiti in our subways. Comissioner [Last Name from 4>],
+      |please take the guilty party away.
+      |""".stripMargin
+
+  val text5a: String =
+    """
+      |Commissioner [Last Name from 4>] and the two policemen assist
+      |you out of the subway car and down several flights of
+      |foulsmelling steps to the underground tattoo parlor of Tarantula
+      |Jack. There, as the policemen hold you down, Commissioner
+      |(name) tells Tarantula Jack that your forehead
+      |is to be tattooed with the same words you sprayed on the
+      |window of the subway car. Your struggles are useless as the
+      |tattooist 's buzzing needle sets forth its everlasting
+      |reminder of a punishment truly suited to its crime. When the
+      |work is done, Commissioner (name holds up a mirror to your face--
+      |and you wake, screaming.
+      |""".stripMargin
+
+  val text6: String =
+    """
+      |You spray a giant X across the window of the subway car, then
+      |return the spray can to the figure who had given it to you--
+      |and who is no longer faceless. Yet the face he now has is
+      |somehow more frightening than his earlier facelessness--for
+      |it is your own face. He has taken it from you, along with
+      |your name, and left you nothing but this scrawl on the
+      |subway window. You press your hands to the featureless ovoid
+      |that grows from the stalk of your neck and try, mouthlessly,
+      |to scream. You wake, trembling and covered with sweat_
+      |""".stripMargin
+
+  val text7: String =
+    """
+      |The subway car screeches to a stop at 34th STREET, where you
+      |are able to enter Oldman's Department Store directly from the
+      |subway platform. "I'll have to leave you here," your
+      |companion tells you, "but the Personnel Office is on the 13th
+      |floor. And there--" His featureless head nods toward the
+      |purring escalator at the center of the deserted sales floor.
+      |"--is the escalator. See you later. . . X."
+      |""".stripMargin
+
+  val text7a: String =
+    """
+      |You try to (response)--but you can't. Your acts don't seem to
+      |be under your own control. An elderly floorman approaches you
+      |and asks if you are looking for the escalator. You nod.
+      |He points his bony finger toward the purring, gliding steps.
+      |"It's right there, sir," he informs you.
+      |""".stripMargin
+
+  val text8: String =
+    """
+      |You take the escalator up to the main sales floor, which
+      |smells rather cloyingly of perfume. An elderly saleswoman
+      |smiles at you from behind a cosmetics counter--and points at
+      |the ascending escalator.
+      |""".stripMargin
+
+  val text9: String =
+    """
+      |You take the escalator to the second floor, where four
+      |female mannequins have been grouped in a tableau representing
+      |an outing to the beach. Each of the mannequins has lifted her
+      |plaster hand to point to the upward-bound escalator.
+      |""".stripMargin
+
+  val text10: String =
+    """
+      |You take the escalator to the third floor, which is devoted
+      |to displays of men's fashions. On the counter just before
+      |you a single leather glove on sale for $12.95 points to the
+      |Up escalator.
+      |""".stripMargin
+
+  val text11: String =
+    """
+      |You take the escalator to the fourth floor, where a placard
+      |informs you that the Les Delices has been closed for renovation.
+      |Another placard shows a hand pointing, with no explanation toward
+      |the Up escalator.
+      |""".stripMargin
+
+  val text12: String =
+    """
+      |You take the escalator to the fifth floor, where a white-haired
+      |salesman stands daydreaming behind a counter displaying all kinds of
+      |cutlery. "Could I interest you in a knife, Sir?" he asks wistfully.
+      |""".stripMargin
+
+  val text12a: String =
+    """
+      |"Very good, Sir. This--" He holds a a knife with an flinch
+      |stainless steel blade to your throat. "--is our very best
+      |all-purpose carving knife. And this--" The carving knife drops
+      |from his hand, and he takes another, smaller knife from the counter.
+      |"This is a superb knife for boning chicken." He lunges at you with
+      |the knife, which makes a long gash in the sleeve of your white coat--
+      |but does no more significant harm.
+      |""".stripMargin
+
+  val text12b: String =
+    """
+      |"No? You won't even look at my knives?" The whitehaired salesman sighs.
+      |"I don't know why I waste my time. All these years, and all these knives,
+      |and never once... never once..." He picks up the largest of the knives
+      |from the counter and, with a really remarkable steadiness of purpose,
+      |slowly positions it over the left-hand breast pocket of his suit and
+      |commits suicide. "I'm sorry," he says, with his last dying breath. "I tried
+      |to be a good salesman. I did... my level... best."
+      |""".stripMargin
+
+  val text13: String =
+    """
+      |You take the escalator to the sixth floor, where the
+      |management of Oldman 's announces, on a large poster that it
+      |is proud to be selling, in cooperation with the Sistine
+      |Chapel, a collection of priceless Fine Art Reproductions, in-
+      |cluding a gigantic full-color reproduction of the Hand of God
+      |from the ceiling of the Sistine Chapel. The Hand of God is
+      |pointing to the Up escalator.
+      |""".stripMargin
+
+  val text14: String =
+    """
+      |You take the escalator to the seventh floor, which seems to
+      |be an empty warehouse. Luxuriant growths of cobwebs festoon
+      |the light fixtures. Unmarked boxes and bundles are piled
+      |everywhere. The disembodied arm of a mannequin lies in the
+      |dust, its finger pointing with modest insistence to the UP
+      |escalator.
+      |""".stripMargin
+
+  val text15: String =
+    """
+      |You take the escalator to the eighth floor, where Oldman's
+      |Hair-Styling Saloon is situated. "Hello!" says the chief
+      |hair stylist, an elderly man with a waxed mustache like the
+      |artist Salvador "I see we have our work cut out for us
+      |today! Sit down, please." He gestures toward a low chair
+      |next to the shampooing sink. You shake your head. You don't
+      |want a shampoo. The hair stylist insists.
+      |""".stripMargin
+
+  val text15a: String =
+    """
+      |"This wont take more than five or six hours," the aged hair
+      |stylist assures you. "We simply have to remove all these
+      |facial growths and seal these unsightly pores with sealing
+      |wax and then fill in these repulsive cavities. My, what
+      |large nostrils you have! But with your nose removed they
+      |won 't be a problem any longer. Then we 'll take care of your
+      |eyes with some industrial-strength eye-cover. The better
+      |stores these days prefer mannequins with perfectly blank
+      |faces.  Eyes are out, didn't you know that?"
+      |""".stripMargin
+
+  val text15b: String =
+    """
+      |While the old hair stylist chattered away, his clippers and
+      |trimmers and gaugers and sanders and sealers clipped and
+      |trimmed and gouged and sanded and sealed until, just as he'd
+      |promised, you have been completely remodeled in the new blank
+      |style. "Now, isn't that a lot better," the old man says,
+      |holding up a mirror for you to see your now so much more
+      |geometrical face. "I'm sure the Personnel Department will
+      |hire you right off the bat--and assign you to work in one of
+      |the front windows. Well, have a nice day." And he paints
+      |you toward the Up escalator.
+      |""".stripMargin
+
+  val text16: String =
+    """
+      |You take the escalator up to the ninth floor, where an
+      |elderly salesman insists on giving you a demonstration of the
+      |[Name of computer for which the disc is adapted] computer.
+      |The salesman shows you how easy it is to boot a disc, and
+      |then some words flicker down the face of the screen. "That
+      |is a riddle," the salesman explains, "and you must solve it."
+      |The lines on the screen are:
+      |                 Although I talk of no one and
+      |                 Of nothing else but me and mine,
+      |                 I hope you will not understand
+      |                 Just who I am until the line
+      |                 Revealing all my taradiddle
+      |                 As the substance of _______.
+      |""".stripMargin
+
+  val text16a: String =
+    """
+      |I'm afraid that's not the answer. It's a very simple
+      |riddle really. Almost everyone gets it right away. Keep
+      |trying.
+      |""".stripMargin
+
+  val text16b: String =
+    """
+      |"Very good! You see how easy it is to use a computer? And
+      |when you 've finished, just remove the disc from the disc
+      |drive, like so--" But instead of removing the computer's
+      |disc, his fingers open a flap in your right side, just
+      |beneath your liver, and he removes your own software. "The,
+      |program stays on ROM--that is, on Read-Only Memory--until you
+      |throw the switch. Now, where do they put the switch on this
+      |model?"
+      |""".stripMargin
+
+  val text16c: String =
+    """
+      |"You 've got it? Good: I'll switch it off." He reaches
+      |behind your neck, and the last thing you remember are his
+      |fingers on the switch of consciousness as he turns you off.
+      |You awake with a cry of protest.
+      |""".stripMargin
+
+  val text17: String =
+    """
+      |You take the escalator up to the tenth floor, which seems to
+      |be an assembly area for the store mannequins. Some stand in
+      |front of full-length mirrors trying on and taking off different
+      |styles and positions of limbs. "Hello,"says one |particularly
+      |attractive blonde, jutting her hip to the side, in a traditional
+      |posture of greeting. "My name `s Hugette, what 's yours?" You try
+      |to answer her question, but you appear to have lost the use of your
+      |voice. Hugette seems not to notice. "My full name," she continues,
+      |"is Hugette Wadju-Paiffer, with a hyphen. You have a very attractive
+      |head. Do you mind if I try it on?"
+      |""".stripMargin
+
+  val text17a: String =
+    """
+      |Taking your silence as her permission, Hugette takes a good
+      |grip on your head and slowly unscrews it from your neck.
+      |Then she gives it to you to hold while she tries to unscrew
+      |her own head. "Oh dear," she complains. "It 's stuck! Help
+      |me, won't you?" You set your head down on the counter behind
+      |you and take a firm grip on Hugette's head and try to twist
+      |it loose, but s stuck to her neck as solidly as the cap on
+      |a jar of pickles. "Stop!" she shrieks. You stop twisting--
+      |and then realize she did not mean you. She was yelling at
+      |the mannequin who has taken your head from the counter while
+      |your back was turned and is now running away with it up the
+      |escalator.
+      |""".stripMargin
+
+  val text18: String =
+    """
+      |You run up the escalator's moving steps to the eleventh
+      |floor and arrive on the sales floor just in time to see the
+      |mannequin with your head under his arm taking the steps of
+      |the upbound escalator two at a time. A burglar alarm begins
+      |to shree. An aged security officer catches hold of your
+      |wrist and asks you where the fire is. The only answer you
+      |can give, voiceless as you are, is to point to the escalator
+      |where the thieving mannequin bears away your head in triumph.
+      |"I'm sorry, young man," says the security officer. "But we
+      |can't have people going about the store without their heads
+      |or their shoes. That's the rule here at Oldman's. You'll
+      |have to come along with me."
+      |""".stripMargin
+
+  val text18a: String =
+    """
+      |The security officer takes you to a fitting room at the
+      |side of the sales floor and deftly fits a spare head into the
+      |empty socket in your neck. You look at yourself in the
+      |mirror. To your surprise you are now a man of sixty or
+      |seventy years, and a cousin if not the twin of the security
+      |officer, who welcomes you with a smile to your new place of
+      |employment. At last you have a mouth and are able--even as
+      |you wake from the nightmare--to scream.
+      |""".stripMargin
+
+  val text19: String =
+    """
+      |You won't? You won't!" he shouts at you. But you've
+      |already broken his grip and are running up the escalator to
+      |the twelfth floor, which is given over to Oldman's Shipping
+      |Department. None of the department's staff is anywhere in
+      |sight. You are standing in the midst of hundreds of boxes
+      |of all shapes and colors, each stamped with Oldman's ornate
+      |monogram. Faintly, from one of those boxes, you can hear
+      |your head calling to you: "Help! Help me get out of this
+      |box. I'm suffocating. Help!" Your voice grows weaker, and
+      |your own strength is ebbing rapidly. It seems so unfair--to
+      |have got this close to the Personnel Department and then to
+      |fail. You tell yourself you must find your head and take it
+      |up the last flight of steps to be interviewed.
+      |""".stripMargin
+
+  val text20: String =
+    """
+      |You open the box nearest at hand. It contains a ceramic
+      |vase, jade green with dark specklings. It won't do for a
+      |head.
+      |""".stripMargin
+
+  val text20a: String =
+    """
+      |You open another box. It contains a basket imported from
+      |Thailand. It 's just about the right size for carrying your
+      |head, once you find it, but that's small consolation.
+      |""".stripMargin
+
+  val text20b: String =
+    """
+      |You open a third box. It contains a large Gouda cheese
+      |from the Gourmet Grocery Department.
+      |""".stripMargin
+
+  val text20c: String =
+    """
+      |You open yet another box. It contains lingerie in a style
+      |you would not have supposed Oldman's would stock. You begin
+      |to feel discouraged. It's been several minutes since you've
+      |heard so much as a whimper from your detached cranium.
+      |""".stripMargin
+
+  val text20d: String =
+    """
+      |You open a fifth box, which seems too small to contain your
+      |head. But there it is, still alive! Its eyes look up to you
+      |gratefully. Its lips smile. And then, with horror, you
+      |realize your mistake. This isn't your own head. It's an
+      |identical head that's been substituted for your own. This
+      |head belongs to... to... His name is on the tip of
+      |your tongue. But of course without a head you are also
+      |without a tongue. You wake, gasping for breath, and
+      |instantly the nightmare fades from your memory.
+      |""".stripMargin
+}
+
